de02a5b94b8b28b4d0f0bf72a7b883ed00146803
kstefanowicz <kevinstefanowicz@cox.net>
2025-04-03 09:19:00 -0400

    drag-to-scroll, horizontal wheel scroll

--- src/views/AnimView.vue
+++ src/views/AnimView.vue
@@ -6,10 +6,63 @@ import anime from 'animejs';
 
 
 
-const numList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+const numList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13']
 const animStore = useAnimStore()
 // init null list same size as numList
 const animatingCards = reactive(numList.map(() => null))
+
+const container = ref(null)
+const isDragging = ref(false)
+const startX = ref(0)
+const dragX = ref(0)
+const dragStartTime = ref(0)
+
+// #region Drag - to - scroll
+const startDrag = (e) => {
+    isDragging.value = true
+    startX.value = e.pageX
+    dragX.value = container.value.scrollLeft
+    dragStartTime.value = Date.now()
+
+    document.body.style.cursor = 'grabbing'
+}
+
+const endDrag = () => {
+    isDragging.value = false
+    document.body.style.cursor = 'default'
+}
+
+const moveDrag = (e) => {
+    if (isDragging.value) {
+        e.preventDefault()
+        container.value.scrollLeft = dragX.value - (e.pageX - startX.value)
+    }
+}
+// #endregion
+
+const handleWheel = (e) => {
+    e.preventDefault()
+    const diff = container.value.scrollLeft + e.deltaY * 2
+    anime({
+        targets: container.value,
+        scrollLeft: diff,
+        duration: 100,
+        easing: 'easeOutQuad'
+    })
+}
+
+const clickCard = (e) => {
+    console.log("Clicked", e.target.getAttribute('data-index'))
+    anime({
+        targets: e.target,
+        rotate: 360,
+        duration: 2000,
+        easing: 'easeOutExpo',
+        complete: () => {
+            e.target.rotation = 0
+        }
+    })
+}
 
 const animCard = (e, isUp) => {
     const card = e.target
@@ -32,6 +85,7 @@ const animCard = (e, isUp) => {
 
 }
 
+
 </script>
 
 <template>
@@ -41,9 +95,10 @@ const animCard = (e, isUp) => {
     <div class="anim-header">
         <h1>Playground for animation</h1>
     </div>
-    <div class="container">
+    <div class="container" ref="container" @mousedown="startDrag" @mousemove="moveDrag" @mouseup="endDrag"
+        @mouseleave="endDrag" @wheel="handleWheel">
         <div v-for="(num, index) in numList" class="num-card" :key="index" :data-index="index"
-            @mouseenter="(e) => animCard(e, true)" @mouseleave="(e) => animCard(e, false)">
+            @mouseenter="(e) => animCard(e, true)" @mouseleave="(e) => animCard(e, false)" @click="clickCard">
             {{ num }}
         </div>
     </div>
@@ -61,7 +116,11 @@ const animCard = (e, isUp) => {
 .container {
     display: flex;
     margin: 0 auto;
-    border: 1px dashed
+    border: 1px dashed;
+    overflow-x: auto;
+    overflow-y: visible;
+    scrollbar-width: none;
+    padding-top: 100px;
 }
 
 .num-card {
