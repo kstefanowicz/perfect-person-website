8bb78476d05d73c066cf8053734907528c843e0b
kstefanowicz <kevinstefanowicz@cox.net>
2025-04-07 11:59:52 -0400

    update Vite

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/perfect-person-website',
   plugins: [
     vue(),
     vueDevTools(),
