877d8c3640877e21203266da2db9a0f9121a8895
kstefanowicz <kevinstefanowicz@cox.net>
2025-04-08 06:16:55 -0400

    extract grid to component

--- src/components/PodcastGrid.vue
+++ src/components/PodcastGrid.vue
@@ -1,9 +1,62 @@
 <script setup>
+import { useSelectionStore } from '@/stores/selectionStore'
+import { useAnimStore } from '@/stores/animStore'
+import { ref } from 'vue'
+import PodcastEp from '../components/PodcastEp.vue'
+
+const props = defineProps({
+    handleCardClick: Function
+})
+
+const selectionStore = useSelectionStore()
+const animStore = useAnimStore()
+
+const podCardWidth = ref('300px')
+const podCardHeight = ref('200px')
 
 </script>
 
 <template>
-
+    <div class="podcast-grid">
+        <PodcastEp v-for="episode in selectionStore.episodes" :key="episode.id" :podcast-episode="episode"
+            :class="['podcast-card', { 'selected': selectionStore.isSelected(episode.id) }]" :data-id="episode.id"
+            @click="handleCardClick(episode.id)" />
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.podcast-grid {
+    display: grid;
+    z-index: 1;
+    grid-template-columns: repeat(auto-fill, minmax(v-bind(podCardWidth), 1fr));
+
+}
+
+.podcast-card {
+    padding: 20px;
+    width: v-bind(podCardWidth);
+    height: v-bind(podCardHeight);
+    border: 2px solid white;
+}
+
+.podcast-card:hover:not(.selected) {
+    padding: 18px;
+    border: 4px solid var(--pp-lightred);
+    border-radius: .5em;
+    cursor: pointer
+}
+
+.selected-podcast-container {
+    display: flex;
+    z-index: v-bind('selectionStore.isShowcaseVisible ? 10 : -1');
+    align-items: center;
+    min-height: 400px;
+}
+
+.selected {
+    padding: 16px;
+    border: 6px solid var(--pp-red);
+    border-radius: .5em;
+    cursor: pointer
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,14 +5,12 @@ import SiteHeader from '../components/SiteHeader.vue'
 import ShowcaseContainer from '@/components/ShowcaseContainer.vue'
 import { useSelectionStore } from '@/stores/selectionStore'
 import { useAnimStore } from '@/stores/animStore'
+import PodcastGrid from '@/components/PodcastGrid.vue'
 
 const selectionStore = useSelectionStore()
 const animStore = useAnimStore()
 
 const showcaseContainer = ref(null)
-
-const podCardWidth = ref('300px')
-const podCardHeight = ref('200px')
 
 function handleCardClick(id) {
 
@@ -67,11 +65,7 @@ function handleCardClick(id) {
       <ShowcaseContainer v-show="selectionStore.isShowcaseVisible" :key="selectionStore.selectedEpisode?.id"
         :podcast-episode="selectionStore.selectedEpisode" :class="['selected-ep-card']" @close="handleCardClick(-1)" />
     </div>
-    <div class="podcast-grid">
-      <PodcastEp v-for="episode in selectionStore.episodes" :key="episode.id" :podcast-episode="episode"
-        :class="['podcast-card', { 'selected': selectionStore.isSelected(episode.id) }]" :data-id="episode.id"
-        @click="handleCardClick(episode.id)" />
-    </div>
+    <PodcastGrid :handle-card-click="handleCardClick" />
   </main>
 
 </template>
@@ -101,40 +95,5 @@ main {
   color: white;
   display: block;
   padding: 0 auto
-}
-
-.podcast-card {
-  padding: 20px;
-  width: v-bind(podCardWidth);
-  height: v-bind(podCardHeight);
-  border: 2px solid white;
-}
-
-.podcast-card:hover:not(.selected) {
-  padding: 18px;
-  border: 4px solid var(--pp-lightred);
-  border-radius: .5em;
-  cursor: pointer
-}
-
-.selected-podcast-container {
-  display: flex;
-  z-index: v-bind('selectionStore.isShowcaseVisible ? 10 : -1');
-  align-items: center;
-  min-height: 400px;
-}
-
-.selected {
-  padding: 16px;
-  border: 6px solid var(--pp-red);
-  border-radius: .5em;
-  cursor: pointer
-}
-
-.podcast-grid {
-  display: grid;
-  z-index: 1;
-  grid-template-columns: repeat(auto-fill, minmax(v-bind(podCardWidth), 1fr));
-
 }
 </style>
