1ce6c9f8841cf0e2dd559eb60141e2ae67c9f266
kstefanowicz <kevinstefanowicz@cox.net>
2025-04-08 06:56:32 -0400

    add rickroll to showcase

--- src/components/ShowcaseContainer.vue
+++ src/components/ShowcaseContainer.vue
@@ -15,6 +15,11 @@ watchEffect(() => {
 <template>
     <div class="showcase-frame">
         <div class="youtube-embed">
+            <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=9JFkdro8Hl-boQqJ" title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
         </div>
         <div class="episode-card">
             <h2 class="color-red">{{ showcaseEp?.title }}</h2>
@@ -43,6 +48,11 @@ watchEffect(() => {
     min-height: 150px;
     background: black;
     aspect-ratio: 16 / 9;
+}
+
+iframe {
+    width: 100%;
+    height: 100%
 }
 
 .episode-card {
