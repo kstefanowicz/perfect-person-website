de87775ce02c726e96a15a3026c4f545fd9af75f
kstefanowicz <kevinstefanowicz@cox.net>
2025-04-08 08:30:28 -0400

    remove redundant array unstringification

--- src/components/PodcastEp.vue
+++ src/components/PodcastEp.vue
@@ -4,29 +4,12 @@ const props = defineProps({
     podcastEpisode: Object
 })
 
-//console.log(props.podcastEpisode.thumbnail)
-
-const thumbnails = computed(() => {
-    if (Array.isArray(props.podcastEpisode.thumbnail)) {
-        return props.podcastEpisode.thumbnail
-    }
-
-    try {
-        var out = JSON.parse(props.podcastEpisode.thumbnail)
-        console.log("OUT - " + out[1].url)
-        return out
-    } catch (e) {
-        console.error(e)
-        return []
-    }
-})
-
 </script>
 
 <template>
     <div>
         <div v-if="podcastEpisode && podcastEpisode.thumbnail">
-            <img :src="thumbnails[2]?.url" />
+            <img :src="podcastEpisode.thumbnail[2]?.url" />
         </div>
         <h3 class="podcast-ep-title color-blue"><strong>{{ podcastEpisode.title }}</strong></h3>
         <p class="podcast-ep-length color-red">{{ podcastEpisode.length }}</p>
