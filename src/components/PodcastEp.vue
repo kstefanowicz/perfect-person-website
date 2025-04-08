4ef7bcb4db7ce66432a2678db945f24b45d924ed
kstefanowicz <kevinstefanowicz@cox.net>
2025-04-08 08:14:24 -0400

    add thumbnail to PodcastEp

--- src/components/PodcastEp.vue
+++ src/components/PodcastEp.vue
@@ -1,12 +1,34 @@
 <script setup>
-defineProps({
+import { computed } from 'vue'
+const props = defineProps({
     podcastEpisode: Object
 })
+
+//console.log(props.podcastEpisode.thumbnail)
+
+const thumbnails = computed(() => {
+    if (Array.isArray(props.podcastEpisode.thumbnail)) {
+        return props.podcastEpisode.thumbnail
+    }
+
+    try {
+        var out = JSON.parse(props.podcastEpisode.thumbnail)
+        console.log("OUT - " + out[1].url)
+        return out
+    } catch (e) {
+        console.error(e)
+        return []
+    }
+})
+
 </script>
 
 <template>
     <div>
-        <h3 class="podcast-ep-title color-blue"><strong>{{ podcastEpisode.title }}</strong></h3>
+        <div v-if="podcastEpisode && podcastEpisode.thumbnail">
+            <img :src="thumbnails[2]?.url" />
+        </div>
+        <h5 class="podcast-ep-title color-blue"><strong>{{ podcastEpisode.title }}</strong></h5>
         <p class="podcast-ep-length color-blue">{{ podcastEpisode.length }}</p>
     </div>
 
